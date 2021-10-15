3d292e2c7e718060cc3304d25f20e42e12e129f7
ricardo-limaco <ricardolimaco.05@gmail.com>
2021-10-15 17:04:29 +0200

    JS 2

--- script-2.js
+++ script-2.js
@@ -1,13 +1,13 @@
 const array1 = [15, 2, 7];
 const array2 = [5];
-const numero = 1;
+const numero = 9;
 
 // creo ciclo for del Array1
 for (i = 0; i < array1.length; i++) {
   // Creo variabile di array[1]
   const incremento = array1[i]
 
-  if (array2 <= incremento);
+  if (array2 < incremento);
   // se array2 è minore di array1, aumento il suo numero
   array2.push(numero);
 
